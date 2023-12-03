c4124519118ed95d2c4c77ebfcc28c70ff369bc3
Andrej Sipka <andrei.sipka@gmail.com>
2023-12-03 22:33:34 +0100

    home page init layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'portfolio-home',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+   selector: 'app-home',
+   standalone: true,
+   imports: [CommonModule],
+   templateUrl: './home.component.html',
+   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent { }
