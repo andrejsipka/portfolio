a0b6c4176904f06e2dd688504c9370e315ba518b
Andrej Sipka <andrei.sipka@gmail.com>
2023-12-27 19:00:04 +0100

    Removed lava background from the project

--- src/app/home/feature/home.component.ts
+++ src/app/home/feature/home.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LavaBackgroundComponent } from '../ui/lava-background/lava-background.component';
 import { RouterModule } from '@angular/router';
 import { ProjectCardComponent } from '../ui/project-card/project-card.component';
 import { ArticleItemComponent } from '../../shared/ui/article-item/article-item.component';
 import { BlogService } from '../data-access/blog.service';
 import { Observable } from 'rxjs';
 import { ProjectService } from '../data-access/project.service';
+import { PostsService } from 'src/app/shared/data-access/posts.service';
 
 @Component({
    selector: 'app-home',
    standalone: true,
    imports: [
       CommonModule,
-      LavaBackgroundComponent,
       RouterModule,
       ProjectCardComponent,
       ArticleItemComponent
@@ -24,11 +23,17 @@ import { ProjectService } from '../data-access/project.service';
 export class HomeComponent implements OnInit {
    protected blogService = inject(BlogService);
    protected projectService = inject(ProjectService);
+   protected postsService = inject(PostsService);
 
    public posts$: Observable<any[]> = this.blogService.getRecentBlogs();
    public projects$: Observable<any[]> = this.projectService.getProjects();
+
+   constructor() {
+      this.postsService.getPosts();
+   }
    
    public ngOnInit(): void {
       this.blogService.getRecentBlogs();
+
    }
 }
